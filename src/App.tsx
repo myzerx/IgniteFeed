22fd4c5b41963d602bbedf34abee6cd7e6492c24
richardlemos <richard.lemos@lab2dev.com>
2023-05-04 15:09:31 -0300

    feat: delete comments, likeCount feats

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+//Named Export
+import { Header } from './components/Header';
+import {Post, PostType} from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://github.com/myzerx.png',
+      name: 'Richard Lemos',
+      role: 'SAP BTP Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Bom dia!',},
+      { type: 'paragraph', content: 'Tudo certo?',},
+      { type: 'link' , content:'jane.design/doctorcare',}          
+    ],
+    publishedAt: new Date('2023-03-30 10:43:00')
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://github.com/grcamargo.png',
+      name: 'Gustavo Camargo',
+      role: 'Backend Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link' , content:'jane.design/doctorcare',}          
+    ],
+    publishedAt: new Date('2023-03-30 7:24:00')
+  },
+];
+
+export function App() {
+  return (
+   <div>
+    <Header />
+
+    <div className={styles.wrapper}>
+    <Sidebar />
+      <main>
+        {posts.map(post => {
+          return (
+            <Post 
+              key={post.id}
+              post={post}
+            />
+          )
+        })}   
+      </main>
+    </div>  
+   </div>
+  )
+}
+
+
---
+++ src/components/Comment.tsx
@@ -0,0 +1,50 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+import { useState } from 'react'
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment}: CommentProps){
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+    
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src='https://github.com/myzerx.png' alt=""/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Richard Lemos</strong>
+                            <time title="30 de Março as 6:51" dateTime=''>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
